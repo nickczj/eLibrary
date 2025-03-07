013d8f2dbb029a5f72e22438f35d046309064d74
nickczj <nick.chow.zj@gmail.com>
2025-03-07 20:08:08 +0800

    remove unused Logger2 function

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -2,37 +2,11 @@ package middleware
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 	"io"
 	"time"
 )
-
-func Logger2() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		startTime := time.Now()
-
-		// Log request details
-		bodyBytes, _ := io.ReadAll(c.Request.Body)
-		c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes)) // Reset request body
-		fmt.Printf("Incoming Request: %s %s\nHeaders: %v\nBody: %s\n",
-			c.Request.Method, c.Request.URL.Path, c.Request.Header, string(bodyBytes))
-
-		// Capture response body
-		responseBody := &bytes.Buffer{}
-		writer := &responseWriter{ResponseWriter: c.Writer, body: responseBody}
-		c.Writer = writer
-
-		// Process the request
-		c.Next()
-
-		// Log response details
-		endTime := time.Since(startTime)
-		fmt.Printf("Response Status: %d\nResponse Body: %s\nResponse Time: %v\n\n",
-			writer.Status(), responseBody.String(), endTime)
-	}
-}
 
 func Logger() gin.HandlerFunc {
 	return func(c *gin.Context) {
